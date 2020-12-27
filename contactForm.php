8049dd34a4bda7f3ab3a10bd4027596124db1d6c
achirasarker <67404429+achirasarker@users.noreply.github.com>
2020-12-27 15:01:29 -0500

    Contact Form

---
+++ contactForm.php
@@ -0,0 +1,20 @@
+<?php
+
+  if(isset($_POST['submit'])) {
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $recipient = "a7sarker@uwaterloo.ca";
+    $header = "From: $email \r\n";
+    $formcontent="From: $name \n Phone: $phone \n\n Message: $message";
+
+    mail($recipient, $subject, $formcontent, $header) or die("Error!");
+    echo "Thank You!" . "-" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'>Return Home</a>";
+
+  }
+
+?>
